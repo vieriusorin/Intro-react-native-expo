2bb3d57af4e64c5505442f9330028d876f201fb4
sorin vieriu <sorinvieriu@gmail.com>
2025-06-16 16:14:30 +0300

    intial commit

---
+++ theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+    colors: {
+        primary: '#7952b3',
+        secondary: '#6c757d',
+        success: '#198754',
+        danger: '#dc3545',
+        warning: '#ffc107',
+        info: '#0d6efd',
+        light: '#f8f9fa',
+        dark: '#212529',
+    }
+}
